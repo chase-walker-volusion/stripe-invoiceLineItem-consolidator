66bcf2c1b65f64eac92687b63dd78b35802843d0
Chase Walker <chase.walker@volusion.com>
2020-05-21 15:27:47 -0500

    added addParams and queryJob functions

--- bigQuery.py
+++ bigQuery.py
@@ -2,7 +2,8 @@ from google.cloud import bigquery
 
 client = bigquery.Client()
 
-# Perform a query.
+lineItemTotal = 0
+
 QUERY = ("""
     SELECT line_item_subscription,id, invoice_date, count(distinct line_item_id) as line_item_count FROM `v1-dev-main.stripe.vw_LatestInvoices_lineitems` 
     WHERE lower(line_item_description) like '%ship%cost%label%'
@@ -13,12 +14,37 @@ QUERY = ("""
 """)
 QUERY2 = ("""
     SELECT * FROM `v1-dev-main.stripe.vw_LatestInvoices_lineitems`
-    WHERE id = 'in_1GjB9nJXC8AX4nQMj9z2PnNJ'
-    AND line_item_subscription = 'sub_ELeCi1zPEqGisC'
+    WHERE id = 'invoiceID'
+    AND line_item_subscription = 'subscriptionID'
     ORDER BY line_item_id
 """)
-query_job = client.query(QUERY)  # API request
-rows = query_job.result()  # Waits for query to finish
 
-for row in rows:
-    print(row)
+# adds any number of kwargs to replace variables in a given query
+# key should be the same as the text variable within the query
+def addParams(query, **kwargs):
+    for key, value in kwargs.items():
+        query = str.replace(query, key, value)
+    return query
+
+
+# take a SQL query string and returns the query result from BQ
+# also prints out each row returned, and adds the amount for the row to lineItemTotal
+def queryJob(query):
+    global lineItemTotal
+    query_job = client.query(query) 
+    rows = list(query_job.result())
+    for row in rows:
+        print(row)
+        if len(row) > 4: # query1 is only len 4, so anything larger is query2
+            lineItemTotal += row[2] # row[2] is amount in query2
+    return rows
+
+query1 = queryJob(QUERY)
+
+# loops through each row in query1 and adds the returned params (subscriptionID, invoiceID)
+# to QUERY2, resulting in newQuery. Each instance of newQuery is then sent to queryJob
+for row in query1:
+    newQuery = addParams(QUERY2, subscriptionID=row[0], invoiceID=row[1])
+    queryJob(newQuery)
+
+print("Total Shipping Cost (in cents): " + str(lineItemTotal))
