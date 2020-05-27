5c93284fea423ffba93ced94503a43383e9255bd
Chase Walker <chase.walker@volusion.com>
2020-05-27 12:13:57 -0500

    Added sum total column to QUERY1

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 QUERY = ("""
-    SELECT line_item_subscription,id, invoice_date, count(distinct line_item_id) as line_item_count FROM `v1-dev-main.stripe.vw_LatestInvoices_lineitems` 
+    SELECT line_item_subscription,id, invoice_date, count(distinct line_item_id) as line_item_count, sum(total) as total FROM `v1-dev-main.stripe.vw_LatestInvoices_lineitems` 
     WHERE lower(line_item_description) like '%ship%cost%label%'
     AND invoice_date >= '2020-05-01'
     GROUP BY
