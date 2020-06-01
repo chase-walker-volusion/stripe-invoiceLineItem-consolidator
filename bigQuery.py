fafdf34d6e4381a09f21973b758f26d5d3b69a6e
Chase Walker <chase.walker@volusion.com>
2020-06-01 13:19:34 -0500

    cleaned up documentation for bigQuery.py

--- bigQuery.py
+++ bigQuery.py
@@ -8,25 +8,35 @@ client = bigquery.Client()
 backupDataSet = client.dataset('dev_chasew')
 backupTable = backupDataSet.table('stripe_lineItem_backup')
 
-
-
 # adds any number of kwargs to replace variables in a given query
-# key should be the same as the text variable within the query
+# TAKES:
+# query (string)
+# **kwargs
+# RETURNS:
+# query (string) w/ updated parameters from **kwargs
 def addParams(query, **kwargs):
     for key, value in kwargs.items():
         query = str.replace(query, key, value)
     return query
 
-
-# take a SQL query string and returns the query result from BQ
-# also prints out each row returned, and adds the amount for the row to lineItemTotal
+# sends SQL query string to BQ and returns the result
+# TAKES:
+# query (string)
+# RETURNS:
+# queryResult (list)
 def queryJob(query):
     query_job = client.query(query) 
-    rows = list(query_job.result())
-    return rows
+    queryResult = list(query_job.result())
+    return queryResult
 
+# sends JSON file to provided BQ table
+# TAKES:
+# uploadFileName (string)
+# table (google.cloud.bigquery.table.TableReference) 
+# RETURNS:
+# jobResult (list)
 def uploadJob(uploadFileName, table):
-    table.reload()
+    table.reload() # pulls the table schema (needs to have already been created)
 
     with open(uploadFileName, 'rb') as uploadFile:
         job = table.upload_from_file(uploadFile, source_format='NEWLINE_DELIMITED_JSON')
@@ -34,22 +44,22 @@ def uploadJob(uploadFileName, table):
     jobResult = list(job.result())
     return jobResult
 
+# list of all invoices that have 5 or more (inclusive) shipping label invoice items
 qualifyingInvoices = queryJob(config.qualifyingInvoicesQuery)
 
-# loops through each row in query1 and adds the returned params (subscriptionID, invoiceID)
-# to QUERY2, resulting in newQuery. Each instance of newQuery is then sent to queryJob
-
+# loops through each invoice in qualifyingInvoices and creates a parameterizedQuery with the subscriptionID and invoiceID provided in the invoice
+# parameterizedQuery is then sent to stripe to grab a backup of the invoice, which is then saved to backup.json
 for invoice in qualifyingInvoices:
-    newQuery = addParams(config.invoiceItemQuery, subscriptionID=invoice[0], invoiceID=invoice[1])
-    invoiceItem = queryJob(newQuery)
+    parameterizedQuery = addParams(config.invoiceItemQuery, subscriptionID=invoice[0], invoiceID=invoice[1])
+    invoiceItem = queryJob(parameterizedQuery)
     invoiceItemID = invoiceItem[0][0]
     stripeBackup = stripe.InvoiceItem.retrieve(invoiceItemID)
+
     with open('backup.json', 'a') as f:
         json.dump(stripeBackup, f)
-    # for rows in query2:
-    #   retrive invoice item record from stripe
-    #   write record to BQ dataset
-    #   request delete from stripe
-    #   
-for backup in stripeBackup:
-    uploadJob('backup.json', backupTable)
+
+    # deletes the invoice item
+    # stripe.InvoiceItem.delete(invoiceItemID)
+
+# uploads the backup.json file to the backupTable
+uploadJob('backup.json', backupTable)
