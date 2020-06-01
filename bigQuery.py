a7c99fc0a7c200f32c5d99db13a2feea3fad393c
Chase Walker <chase.walker@volusion.com>
2020-06-01 12:20:19 -0500

    added uploadJob() and updated main to use it

--- bigQuery.py
+++ bigQuery.py
@@ -1,6 +1,14 @@
 from google.cloud import bigquery
+import json
 import config
+import stripe
+import key
+stripe.api_key = key.key
 client = bigquery.Client()
+backupDataSet = client.dataset('dev_chasew')
+backupTable = backupDataSet.table('stripe_lineItem_backup')
+
+
 
 # adds any number of kwargs to replace variables in a given query
 # key should be the same as the text variable within the query
@@ -17,20 +25,31 @@ def queryJob(query):
     rows = list(query_job.result())
     return rows
 
+def uploadJob(uploadFileName, table):
+    table.reload()
 
-def stripePull(id):
+    with open(uploadFileName, 'rb') as uploadFile:
+        job = table.upload_from_file(uploadFile, source_format='NEWLINE_DELIMITED_JSON')
+        
+    jobResult = list(job.result())
+    return jobResult
 
-
-query1 = queryJob(config.QUERY)
+qualifyingInvoices = queryJob(config.qualifyingInvoicesQuery)
 
 # loops through each row in query1 and adds the returned params (subscriptionID, invoiceID)
 # to QUERY2, resulting in newQuery. Each instance of newQuery is then sent to queryJob
-for row in query1:
-    newQuery = addParams(config.QUERY2, subscriptionID=row[0], invoiceID=row[1])
-    query2  =  queryJob(newQuery)
 
+for invoice in qualifyingInvoices:
+    newQuery = addParams(config.invoiceItemQuery, subscriptionID=invoice[0], invoiceID=invoice[1])
+    invoiceItem = queryJob(newQuery)
+    invoiceItemID = invoiceItem[0][0]
+    stripeBackup = stripe.InvoiceItem.retrieve(invoiceItemID)
+    with open('backup.json', 'a') as f:
+        json.dump(stripeBackup, f)
     # for rows in query2:
     #   retrive invoice item record from stripe
     #   write record to BQ dataset
     #   request delete from stripe
     #   
+for backup in stripeBackup:
+    uploadJob('backup.json', backupTable)
